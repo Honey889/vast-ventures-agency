eebb16727e94636ddd8d4c6a72e74235eff52cc0
Honey -- <hoorulain708@gmail.com>
2024-05-05 14:22:22 +0500

    initial commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,9 +12,9 @@ const Hero = () => {
       <div className='grid grid-cols-1 lg:grid-cols-5 items-center w-[80%] mx-auto'>
 
          <div className='col-span-2'>   
-        <h1 className='text-[27px] md:text-[35px] lg:text-[40px] mb-[1rem] font-bold text-blue-950 leading-[2.4rem] md:leading-[4rem]'>Where Will Your Next Adventure Take You? - Let's Find Out Together!</h1>
+        <h1 className='text-[27px] md:text-[35px] lg:text-[40px] mb-[1rem] font-bold text-blue-950 leading-[2.4rem] md:leading-[4rem]'>Where Will Your Next Adventure Take You? - Let&apos;s Find Out Together!</h1>
 
-        <p className='md:text-[17px] text-[15px] mb-[2rem] text-black opacity-90 font-[400]'>we're passionate about crafting unforgettable travel experiences tailored to your unique preferences and interests. From serene beaches to thrilling adventures and cultural explorations, we're here to turn your travel dreams into reality. </p>
+        <p className='md:text-[17px] text-[15px] mb-[2rem] text-black opacity-90 font-[400]'>We are passionate about crafting unforgettable travel experiences tailored to your unique preferences and interests. From serene beaches to thrilling adventures and cultural explorations, we&apos;re here to turn your travel dreams into reality. </p>
         <div className='flex items-center space-x-4 md:space-x-6'>
             <ButtonBlue text='Book Now'/>
             <ButtonRed text='Discover More'/>
